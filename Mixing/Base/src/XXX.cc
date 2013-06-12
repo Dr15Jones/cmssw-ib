0c74ee8f3f38798e0cd7a30f78bb7ae62a190727
wmtan <wmtan@fnal.gov>
2013-06-12 14:04:30 -0500

    New Development

--- Mixing/Base/src/XXX.cc
+++ Mixing/Base/src/XXX.cc
@@ -1,8 +1,6 @@
 #include "Mixing/Base/src/XXX.h"
 
-#include "DataFormats/Provenance/interface/ProductRegistry.h"
 #include "FWCore/ParameterSet/interface/ParameterSet.h"
-#include "FWCore/Utilities/interface/Algorithms.h"
 
 namespace edm {
   // -----------------------------
@@ -11,113 +9,42 @@ namespace edm {
                      ProductRegistry& preg,
                      ActionTable const& actions,
                      boost::shared_ptr<ProcessConfiguration> processConfiguration) :
-    
-    worker_reg_(boost::shared_ptr<ActivityRegistry>(new ActivityRegistry)),
-    act_table_(&actions),
-    all_workers_(),
-    unscheduled_(new UnscheduledCallProducer) {
-
-    std::vector<std::string> unusedLabels;
+    workerManager_(boost::shared_ptr<ActivityRegistry>(new ActivityRegistry), actions) {
+    std::vector<std::string> shouldBeUsedLabels;
     std::set<std::string> unscheduledLabels;
-    for (auto& pset : psets) {
-	// std::cerr << pset.dump() << std::endl;
-
+    for(auto& pset : psets) {
         std::string label = pset.getParameter<std::string>("@module_label");
-        unusedLabels.push_back(label);
-      //Need to
-      // 1) create worker
-      // 2) if it is a WorkerT<EDProducer>, add it to our list
-        WorkerParams params(pset, &pset, preg,
-                            processConfiguration, *act_table_);
-        Worker* newWorker(worker_reg_.getWorker(params, label));
-        if (newWorker->moduleType() == Worker::kProducer ||
-            newWorker->moduleType() == Worker::kFilter) {
-          unscheduledLabels.insert(label);
-          unscheduled_->addWorker(newWorker);
-          //add to list so it gets reset each new event
-          addToAllWorkers(newWorker);
-        }
+        workerManager_.addToUnscheduledWorkers(pset,
+                                               preg,
+                                               processConfiguration,
+                                               label,
+                                               false,
+                                               unscheduledLabels,
+                                               shouldBeUsedLabels);
     }
-    if (!unscheduledLabels.empty()) {
-      for (ProductRegistry::ProductList::const_iterator it = preg.productList().begin(),
-          itEnd = preg.productList().end();
-          it != itEnd;
-          ++it) {
-        if (it->second.produced() &&
-            it->second.branchType() == InEvent &&
-            unscheduledLabels.end() != unscheduledLabels.find(it->second.moduleLabel())) {
-          it->second.setOnDemand();
-        }
-      }
+    if(!unscheduledLabels.empty()) {
+      workerManager_.setOnDemandProducts(preg, unscheduledLabels); 
     }
-
-    // This is used for a little sanity-check to make sure no code
-    // modifications alter the number of workers at a later date.
-    size_t all_workers_count = all_workers_.size();
-
-    // Sanity check: make sure nobody has added a worker after we've
-    // already relied on all_workers_ being full.
-    assert (all_workers_count == all_workers_.size());
   } // XXX::XXX
   
-  void XXX::endJob() {
-    AllWorkers::iterator ai(workersBegin()), ae(workersEnd());
-    for (; ai != ae; ++ai) {
-      (*ai)->endJob();
-    }
-  }
-
   void XXX::beginRun(RunPrincipal& run, const EventSetup& setup) {
-    processOneOccurrence<OccurrenceTraits<RunPrincipal, BranchActionBegin> >(run, setup);
+    workerManager_.processOneOccurrence<OccurrenceTraits<RunPrincipal, BranchActionBegin> >(run, setup);
   }
 
   void XXX::beginLuminosityBlock(LuminosityBlockPrincipal& lumi, const EventSetup& setup) {
-    processOneOccurrence<OccurrenceTraits<LuminosityBlockPrincipal, BranchActionBegin> >(lumi, setup);
+    workerManager_.processOneOccurrence<OccurrenceTraits<LuminosityBlockPrincipal, BranchActionBegin> >(lumi, setup);
   }
 
   void XXX::endRun(RunPrincipal& run, const EventSetup& setup) {
-    processOneOccurrence<OccurrenceTraits<RunPrincipal, BranchActionEnd> >(run, setup);
+    workerManager_.processOneOccurrence<OccurrenceTraits<RunPrincipal, BranchActionEnd> >(run, setup);
   }
 
   void XXX::endLuminosityBlock(LuminosityBlockPrincipal& lumi, const EventSetup& setup) {
-    processOneOccurrence<OccurrenceTraits<LuminosityBlockPrincipal, BranchActionEnd> >(lumi, setup);
+    workerManager_.processOneOccurrence<OccurrenceTraits<LuminosityBlockPrincipal, BranchActionEnd> >(lumi, setup);
   }
 
   void XXX::setupPileUpEvent(EventPrincipal& ep, const EventSetup& setup) {
-    processOneOccurrence<OccurrenceTraits<EventPrincipal, BranchActionBegin> >(ep, setup);
+    workerManager_.processOneOccurrence<OccurrenceTraits<EventPrincipal, BranchActionBegin> >(ep, setup);
   }
 
-  void XXX::beginJob(ProductRegistry const& iRegistry) {
-    auto const runLookup = iRegistry.productLookup(InRun);
-    auto const lumiLookup = iRegistry.productLookup(InLumi);
-    auto const eventLookup = iRegistry.productLookup(InEvent);
-    for(auto & worker: all_workers_) {
-      worker->updateLookup(InRun,*runLookup);
-      worker->updateLookup(InLumi,*lumiLookup);
-      worker->updateLookup(InEvent,*eventLookup);
-    }
-    
-    for_all(all_workers_, boost::bind(&Worker::beginJob, _1));
-    loadMissingDictionaries();
-  }
-
-  void
-  XXX::resetAll() {
-    for_all(all_workers_, boost::bind(&Worker::reset, _1));
-  }
-
-  void
-  XXX::addToAllWorkers(Worker* w) {
-    if (!search_all(all_workers_, w)) {
-      all_workers_.push_back(w);
-    }
-  }
-
-  void
-  XXX::setupOnDemandSystem(EventPrincipal& ep, EventSetup const& es) {
-    // NOTE: who owns the productdescrption?  Just copied by value
-    unscheduled_->setEventSetup(es);
-    ep.setUnscheduledHandler(unscheduled_);
-  }
-  
 }
--- Mixing/Base/src/XXX.h
+++ Mixing/Base/src/XXX.h
@@ -5,13 +5,8 @@
 
 */
 
-#include "FWCore/Framework/interface/ExceptionHelpers.h"
 #include "FWCore/Framework/interface/Frameworkfwd.h"
-#include "FWCore/Framework/interface/UnscheduledCallProducer.h"
-#include "FWCore/Framework/src/Worker.h"
-#include "FWCore/Framework/src/WorkerRegistry.h"
-#include "FWCore/Utilities/interface/ConvertException.h"
-#include "FWCore/Utilities/interface/Exception.h"
+#include "FWCore/Framework/src/WorkerManager.h"
 
 #include "boost/shared_ptr.hpp"
 
@@ -21,22 +16,10 @@
 namespace edm {
   class XXX {
   public:
-    typedef std::vector<Worker*> AllWorkers;
-
-    typedef std::vector<Worker*> Workers;
-
     XXX(std::vector<ParameterSet>& psets,
              ProductRegistry& pregistry,
              ActionTable const& actions,
              boost::shared_ptr<ProcessConfiguration> processConfiguration);
-
-    template <typename T>
-    void processOneOccurrence(typename T::MyPrincipal& principal,
-                              EventSetup const& eventSetup,
-                              bool cleaningUpAfterException = false);
-
-    void beginJob(ProductRegistry const& iRegistry);
-    void endJob();
 
     void beginRun(RunPrincipal& run, const edm::EventSetup& setup);
     void beginLuminosityBlock(LuminosityBlockPrincipal& lumi, const edm::EventSetup& setup);
@@ -46,82 +29,12 @@ namespace edm {
 
     void setupPileUpEvent(EventPrincipal& ep, const EventSetup& setup);
 
+    void beginJob(ProductRegistry const& iRegistry) {workerManager_.beginJob(iRegistry);}
+    void endJob() {workerManager_.endJob();}
+
   private:
-
-    AllWorkers::const_iterator workersBegin() const {
-      return all_workers_.begin();
-    }
-
-    AllWorkers::const_iterator workersEnd() const {
-      return all_workers_.end();
-    }
-
-    AllWorkers::iterator workersBegin() {
-      return  all_workers_.begin();
-    }
-
-    AllWorkers::iterator workersEnd() {
-      return all_workers_.end();
-    }
-
-    void resetAll();
-
-    void setupOnDemandSystem(EventPrincipal& principal, EventSetup const& es);
-
-    void addToAllWorkers(Worker* w);
-    
-    WorkerRegistry                                worker_reg_;
-    ActionTable const*                            act_table_;
-
-    AllWorkers               all_workers_;
-
-    boost::shared_ptr<UnscheduledCallProducer> unscheduled_;
+    WorkerManager workerManager_;
   };
 
-  template <typename T>
-  void
-  XXX::processOneOccurrence(typename T::MyPrincipal& ep,
-                                 EventSetup const& es,
-                                 bool cleaningUpAfterException) {
-    this->resetAll();
-
-
-    if (T::isEvent_) {
-      setupOnDemandSystem(dynamic_cast<EventPrincipal&>(ep), es);
-    }
-    try {
-      try {
-        try {
-          //make sure the unscheduled items see this transition [Event will be a no-op]
-          unscheduled_->runNow<T>(ep, es);
-        }
-        catch(cms::Exception& e) {
-          actions::ActionCodes action = (T::isEvent_ ? act_table_->find(e.category()) : actions::Rethrow);
-          assert (action != actions::IgnoreCompletely);
-          assert (action != actions::FailPath);
-          if (action == actions::SkipEvent) {
-            printCmsExceptionWarning("SkipEvent", e);
-          } else {
-            throw;
-          }
-        }
-      }
-      catch (cms::Exception& e) { throw; }
-      catch(std::bad_alloc& bda) { convertException::badAllocToEDM(); }
-      catch (std::exception& e) { convertException::stdToEDM(e); }
-      catch(std::string& s) { convertException::stringToEDM(s); }
-      catch(char const* c) { convertException::charPtrToEDM(c); }
-      catch (...) { convertException::unknownToEDM(); }
-    }
-    catch(cms::Exception& ex) {
-      if (ex.context().empty()) {
-        addContextAndPrintException("Calling function XXX::processOneOccurrence", ex, cleaningUpAfterException);
-      } else {
-        addContextAndPrintException("", ex, cleaningUpAfterException);
-      }
-      throw;
-    }
-  }
 }
-
 #endif
