41af2ac8903140951f13ca94fd806d0772e261e3
wmtan <wmtan@fnal.gov>
2013-06-12 16:42:33 -0500

    New Development

--- FWCore/Framework/interface/Schedule.h
+++ FWCore/Framework/interface/Schedule.h
@@ -296,6 +296,8 @@ namespace edm {
     AllOutputWorkers         all_output_workers_;
     TrigPaths                trig_paths_;
     TrigPaths                end_paths_;
+    std::vector<int>         empty_trig_paths_;
+    vstring                  empty_trig_path_names_;
 
     //For each branch that has been marked for early deletion
     // keep track of how many modules are left that read this data but have
@@ -352,6 +354,9 @@ namespace edm {
                                  EventSetup const& es,
                                  bool cleaningUpAfterException) {
     this->resetAll();
+    for (int empty_trig_path : empty_trig_paths_) {
+      results_->at(empty_trig_path) = HLTPathStatus(hlt::Pass, 0);
+    }
     state_ = Running;
 
     // A RunStopwatch, but only if we are processing an event.
